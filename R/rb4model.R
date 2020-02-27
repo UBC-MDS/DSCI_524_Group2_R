4859a56e21741ce86205543d1e4d98345ec42392
Andrea <andrea.lee.011@gmail.com>
2020-02-27 11:47:47 -0800

    update project structure using create package

--- R/rb4model.R
+++ R/rb4model.R
@@ -33,6 +33,26 @@ feature_splitter<-function(x){
 }
 
 
+#' Fit and report
+#'
+#' @param model A machine learning model, can either be classification or regression
+#' @param X The features of the training set
+#' @param y The target of the training set
+#' @param Xv The feature of the validation set
+#' @param yv The target of the validation set
+#' @param m_type The type for calculating error (default = 'regression')
+#'
+#' @return an array of train and validation error
+#'
+#' @description: fits a model and returns the train and validation errors as a list
+#'
+#' @examples
+#'
+#' @export
+fit_and_report <- function(model, X, y, Xv, yv, m_type = 'regression'){
+}
+
+
 # Title     : Feature Selection
 # Objective : Implement forward feature selection and return data with selected features
 # Created by: nowgeun
