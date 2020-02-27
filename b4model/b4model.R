c28b776ca432492de77279e2ac0f4a0024f749c9
Andrea Lee <45249215+andrealee011@users.noreply.github.com>
2020-02-26 23:22:19 -0800

    add missing_val function

---
+++ b4model/b4model.R
@@ -0,0 +1,16 @@
+#' Handles missing values
+#'
+#' Replaces or deletes missing values in a dataframe
+#'
+#' @param df dataframe
+#' @param method string (''delete' deletes row with missing values, 'avg' replaces missing value with the average, 'last' replaces missing value with the last observation) 
+#'
+#' @return dataframe
+#'
+#' @examples
+#' missing_val(df, 'last')
+#'
+#' @export
+missing_val <- function(df, method) {
+  # INSERT CODE HERE
+}
