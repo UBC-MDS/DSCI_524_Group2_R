6b2c0b4cb164bcd5808e35d43dd2fc4482c0a9cd
Sakiga <54461529+eyrakas@users.noreply.github.com>
2020-02-26 23:43:20 -0800

    feature splitter R file added

---
+++ b4model/feature_splitter.R
@@ -0,0 +1,14 @@
+#' feature_splitter function Documentation
+#' Splits dataset column names into a tupple of categorical and numerical lists
+#'
+#' @param x
+#' @return a tupple
+#' 
+#' @examples
+#' feature_splitter(data)
+#' ([categorical:],[numerical: ])
+
+feature_splitter<-function(x){
+  #TODO
+  
+}
