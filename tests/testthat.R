706f99225efce2ff9a44fbbcc7b1bdfee4c57c7a
Andrea Lee <45249215+andrealee011@users.noreply.github.com>
2020-03-03 15:55:14 -0800

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,21 @@
 library(testthat)
 library(rb4model)
 
+test_missing_val <- function() {
+
+  test_that('regression imputation should replace missing value with fitted value', {
+    expect_equal(missing_val(airquality, 'regression')$Ozone[5], -11.7682028889364, tolerance=1e-5)
+  })
+    
+  test_that('mean imputation should replace missing value with average', {
+    expect_equal(missing_val(airquality, 'mean')$Ozone[5], mean(airquality$Ozone, na.rm=TRUE))
+  })
+    
+  test_that('listwise deletion should remove rows with missing values', {
+    expect_equal(nrow(missing_val(airquality, 'delete')), 111)
+  })
+    
+      
+}
+
 test_check("rb4model")
