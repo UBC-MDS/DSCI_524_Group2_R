ed6782c6a305294fd98e74acedcbd6e147c8ec6a
xinwen01 <xinwenwang623@gmail.com>
2020-03-03 21:07:02 -0800

    change fit_and_report

--- b4model/fit_and_report.R
+++ b4model/fit_and_report.R
@@ -1,6 +1,5 @@
-         
-    #' @param model -- A machine learning model, can either be classification or regression
-    #'
+library(mlbench)
+library(caret)
     #' @param X --  The features of the training set
     #'
     #' @param y -- The target of the training set
@@ -9,13 +8,41 @@
     #'
     #' @param yv -- The target of the validation set
     #'
+    #' @param method -- The machine learning method applied 
+    #'
     #' @param m_type-- The type for calculating error (default = 'regression') 
     #'
     #' @return an array of train and validation error
     #'
-    #' @description: fits a model and returns the train and validation errors as a list
+    #' @description: fits a specified model and report the train and test error, only supports model supported by mblench package
     #'
     #' @examples
-    #'fit_and_report(logistic_regression, X, y, Xv, yv)    
-    fit_and_report <- function(model, X, y, Xv, yv, m_type = 'regression'){
-       }
+    #'fit_and_report(logistic_regression, X, y, Xv, yv, method ='glm', m_type = 'classification') 
+    #'   
+fit_and_report <- function(X, y, Xv, yv, method, m_type = 'regression'){
+    if (class(m_type) !='character'){
+        return(exception('The m_type argument should be either regression or classificaition'))
+    }
+    if(len(X) != len(y)){
+        return(exception('The length of X and y should be the same'))
+    }
+    if(len(Xv) != len(yv)){
+        return(exception('The length of Xv and yv should be the same'))
+    }
+    
+    if (startsWith(tolower(m_type), 'regress')){
+        metric <- 'RMSE'
+        model <- train(X, y, method=method, metric=metric)
+        testPred <- predict(model, Xv)
+        test_acc <- postResample(testPred, yv)
+        errors <- c(1 - model$results$RMSE, 1 - test_acc[1] )
+    }
+    if (startsWith(tolower(m_type), 'classif')){
+        metric <-'Accuracy'
+        model<- train(X, y, method=method, metric=metric)
+        testPred <- predict(model, Xv)
+        test_acc <- postResample(testPred, yv)
+        errors <- c(1 - model$results$Accuracy, 1 -test_acc[1]) 
+    }
+    return(errors)
+}
