052a7325aa7dd53827d1013b1540086cc4bdb084
xinwen01 <xinwenwang623@gmail.com>
2020-03-04 11:41:20 -0800

    fit_and_report and test file for review on Wed

--- tests/testthat.R
+++ tests/testthat.R
@@ -10,15 +10,25 @@ test_fit_and_report <- function(){
   y <- dataset$diabetes[1:600]
   xv <- dataset[1:8][600:786,]
   yv <- dataset$diabetes[600:786]
-  result <- fit_and_report(x,y,xv,yv,'glm','classification')
-  print(result)
+  result_c <- fit_and_report(x,y,xv,yv,'glm','classification')
+  data("iris")
+  x1<- iris[1:2][1:100,]
+  x2<-iris[1:2][100:150,]
+  y1<- iris$Petal.Length[1:100]
+  y2<-iris$Petal.Length[100:150]
+  result_r <- fit_and_report(x1,y1,x2,y2,'glm','regression')
+  
   
   test_that("Invalid ouput length", {
-    expect_equal(length(result), 2)
+    expect_equal(length(result_c), 2)
+    expect_equal(length(result_r), 2)
   })
   
   test_that("Score should be in range 0 to 1", {
-    expect_equal(length(result), 2)
+    expect_true(0<=result_c[1] & result_c[1]<=1)
+    expect_true(0<=result_c[2] & result_c[2]<=1)
+    expect_true(0<=result_r[1] & result_r[1]<=1)
+    expect_true(0<=result_r[2] & result_r[2]<=1)
   })
   
   test_that("Error raising failed", {
