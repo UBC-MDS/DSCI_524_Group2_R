0e96a7f5a42948be3ff2379a0d6e844562fa460f
xinwen01 <xinwenwang623@gmail.com>
2020-03-12 18:14:56 -0700

    conflict

--- tests/testthat/test-feature_splitter.R
+++ tests/testthat/test-feature_splitter.R
@@ -7,9 +7,9 @@ test_feature_splitter <- function() {
   Anual_Salary<-c(40000, 65000, 70000, 15000)
   Nationality<-c('Canada', 'USA', 'Britain', 'Australia')
   Marital_Status<-c('Married', 'Single', 'Maried', 'Single')
-  
+
   data <-data.frame(Name, Age, Height, Anual_Salary,Nationality, Marital_Status)
-  
+
   test_that("Result of function MUST be two list", {
     expect_equal(length(feature_splitter(data)), 2)})
   test_that("Test that total number of categorical and numerical features is same as column names in the data frame",{
